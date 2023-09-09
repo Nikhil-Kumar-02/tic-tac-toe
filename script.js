78020b4a7b1d47342d414ab05841e8aa68dad1f0
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2023-09-09 15:05:50 +0530

    now cells get color when won and decolored when reset also now after the match is over no moves can be made by any player

--- script.js
+++ script.js
@@ -37,9 +37,67 @@ class trackMoves{
 
     
     checkForWinner(){
-        if(this.row_0==3||this.row_1==3||this.row_2==3||this.col_0==3||this.col_1==3||this.col_2==3||this.diagonal==3||this.reverseDiagonal == 3)
-        return 1;
-    
+        if(this.row_0==3||this.row_1==3||this.row_2==3||this.col_0==3||this.col_1==3||this.col_2==3||this.diagonal==3||this.reverseDiagonal == 3){
+            // return 1;
+            //if won we also have to color the winner winning moves
+            const checkConditions = [
+                this.row_0==3,
+                this.row_1==3,
+                this.row_2==3,
+                this.col_0==3,
+                this.col_1==3,
+                this.col_2==3,
+                this.diagonal==3,
+                this.reverseDiagonal == 3
+            ];
+            //functions to color them
+            function colorRow(row){
+                for(let col = 0;col<3;col++){
+                    var targetId = ""+row+col;
+                    const cell = document.getElementById(targetId);
+                    cell.style.backgroundColor = "green";
+                }
+            }
+            function colorCol(col){
+                for(let row = 0;row<3;row++){
+                    var targetId = ""+row+col;
+                    const cell = document.getElementById(targetId);
+                    cell.style.backgroundColor = "green";
+                }
+            }
+            function colorDia(offset){
+                for(let i = 0;i<3;i++){
+                    let row = i;
+                    let col = i+offset;
+                    if(col >2)
+                        col = col%2;
+
+                    var targetId = ""+row+col;
+                    const cell = document.getElementById(targetId);
+                    cell.style.backgroundColor = "green";
+                }
+            }
+            //now we can check which among them is correct
+            for(let i = 0;i<checkConditions.length;i++){
+                if(checkConditions[i]){
+                    //this row or col or dia has satisfied
+                    if(i<=2){
+                        colorRow(i);
+                    }
+                    else if(i<=5){
+                        colorCol(i-3);
+                    }
+                    else if(i == 6){
+                        colorDia(0)
+                    }
+                    else{
+                        colorDia(2);
+                    }
+                }
+            }
+            return 1;
+        }
+        
         return 0;
     }
 
@@ -104,18 +162,26 @@ function noteMoves(currPlayer,coordinate){
 
 const button = document.querySelector(".btn");
 
-gridContainer.addEventListener('click' , (e) => {
+function removelistener(){
+    gridContainer.removeEventListener('click',gridCellEvenlistener);
+}
+
+function gridCellEvenlistener(e){
     e.target.innerText = `${currentPlayer}`;
     button.innerText = "Reset Game";
     if(noteMoves(currentPlayer,e.target.id) == 1){
         //will come inside if a player has won the match
         topDisplay.innerText = `Player ${currentPlayer} has won!`;
+        //now even if i click nothing should be displayed on the screen
+        removelistener();
     }
     else{
         togglePlayer();
         displayCurrPlayer();
     }
-})
+}
+
+gridContainer.addEventListener('click' ,gridCellEvenlistener)
 
 button.addEventListener('click' , ()=>{
     //we have to reset the whole game
@@ -124,6 +190,7 @@ button.addEventListener('click' , ()=>{
     // console.log(allBoxes);
     allBoxes.forEach((ele)=>{
         ele.innerText = "";
+        ele.style.backgroundColor = "brown";
     })
     //also undo all players moves 
     playerx.resetCodeBase();
